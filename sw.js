500588ca278ca7372d7f93e4cd8fa3294ed7c5e4
marufhossainkeyas11 <166496971+marufhossainkeyas11@users.noreply.github.com>
2026-06-10 00:24:30 +0600

    Update CACHE_VER to version 2.3.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VER  = 'v2.3.8';
+const CACHE_VER  = 'v2.3.9';
 const SHELL_KEY  = `cs-shell-${CACHE_VER}`;
 const MEDIA_KEY  = `cs-media-${CACHE_VER}`;
 const FONT_KEY   = `cs-fonts-${CACHE_VER}`;
